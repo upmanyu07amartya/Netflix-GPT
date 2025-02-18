bb20bef91d9f67cdb16fb3c62745c8a87ed62085
Amartya Upmanyu <upmanyu07amartya@gmail.com>
2025-02-18 10:37:09 +0530

    initial app structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import "./App.css";
 import React from "react";
+import Body from "./components/Body";
 
 function App() {
 
   return (
-    <div className="text-3xl font-bold text-green-700">
-      <h1>Hello world</h1>
-    </div>
+    <Body />
   );
 }
 
